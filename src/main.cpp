af59013d881aba6d9cf66544a64df22a8cea1885
snpanse <snpanse2@illinois.edu>
2023-08-14 20:21:45 -0400

    Updated pio build and added boilerplate code to src

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,59 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <SparkFun_MS5637_Arduino_Library.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <math.h>
+#include <SPI.h>
+//#include <Arduino_FreeRTOS.h>
+#define BNO055_SAMPLERATE_DELAY_MS (100);
 
+MS5637 pressureSensor;
+
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28,&Wire2);
 
 void setup() {
-  // put your setup code here, to run once:
+  Wire2.begin();
+  Serial.begin(9600);
+  if (!pressureSensor.begin(Wire2))
+  {
+    Serial.println("Could not find a valid MS5637 sensor, check wiring!");
+    while (1);
+  }
+  if(!bno.begin()){
+    Serial.println("Count not find valid BNO055 sensor, check wiring!");
+    while(1);
+  }
+
+  bno.setExtCrystalUse(true);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+
+  float temperature_C = pressureSensor.getTemperature();
+  float pressure_mbar = pressureSensor.getPressure();
+
+  Serial.print("Barometer Temperature (Celsius): ");
+  Serial.println(temperature_C, 2);
+
+  Serial.print("Barometer Pressure (mbar): ");
+  Serial.println(pressure_mbar, 2);
+
+  Serial.print("IMU Orientation: ");
+  Serial.print(event.orientation.x);
+  Serial.print(" ");
+  Serial.print(event.orientation.y);
+  Serial.print(" ");
+  Serial.print(event.orientation.z);
+  Serial.println("");
+
+
+
+  delay(1000); // Wait for 1 second
 }
